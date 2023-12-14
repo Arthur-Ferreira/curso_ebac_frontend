546f5753176b8a791be44e31247bd05bd3d59769
Arthur-Ferreira <arthurfdarosa@gmail.com>
2023-12-14 10:45:17 -0300

    Typescript exercise update

---
+++ 26_Typescript/exercise_ts/main.ts
@@ -0,0 +1,23 @@
+function multi(num1: number, num2: number): number {
+  return num1 * num2
+}
+
+
+
+function hey(name: string): string {
+  return `Olá ${name}`
+}
+
+
+
+console.log(multi(5, 2));
+console.log(multi(2, 3));
+console.log(multi(9, 5));
+console.log(multi(7, 4));
+console.log(multi(1, 8));
+
+console.log(hey('Julia'));
+console.log(hey('Julio'));
+console.log(hey('Brenda'));
+console.log(hey('Bernardo'));
+console.log(hey('Jefferson'));
